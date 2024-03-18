16aed35c34dfe026accabe3e7af216e2d04ad172
HanGyulKang <gang6607@gmail.com>
2024-03-19 00:32:44 +0900

    Initial commit

---
+++ user-service/src/test/java/inflern/study/userservice/UserServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package inflern.study.userservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
