65241010e9883bbb03d0e47c251acb9903dbe382
HanGyulKang <gang6607@gmail.com>
2024-03-28 01:39:39 +0900

    해결은 했는데 이젠 403이 뜬다

--- user-service/src/main/java/inflern/study/userservice/UserServiceApplication.java
+++ user-service/src/main/java/inflern/study/userservice/UserServiceApplication.java
@@ -3,22 +3,12 @@ package inflern.study.userservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @EnableDiscoveryClient
-@RequestMapping("/user-service")
-@RestController
 public class UserServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(UserServiceApplication.class, args);
-	}
-
-	@GetMapping("/actuator/info")
-	public String actuator() {
-		return "user";
 	}
 }
