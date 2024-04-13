8dc104d4a6ef526a8faf3b869971a70d73b87d02
HanGyulKang <gang6607@gmail.com>
2024-04-13 22:07:03 +0900

    user service <-> spring cloud config 서버간 통신을 위한 코드 추가
    - configration 정보가 변경되었을 때 어떻게 대처해야하나?
    1. 서버 재가동 = 큰 의미가 없음. 지양되어야 함
    2. Actuator refresh = 서비스 재부팅 없이 변경된 정보를 가져올 수 있음.
    - Application 상태, 모니터링
    - Metrics 수집을 위한 여러가지 Http-End-Point 를 제공해 줌
    -- /actuator/refresh, /actuator/health, /actuator/beans 등등...
    3. Spring cloud bus 사용 - Actuator 보다 효울적임

--- user-service/src/main/java/inflern/study/userservice/security/WebSecurity.java
+++ user-service/src/main/java/inflern/study/userservice/security/WebSecurity.java
@@ -46,10 +46,13 @@ public class WebSecurity {
         AuthenticationManager authenticationManager = authenticationManagerBuilder.build();
 
         http.csrf(AbstractHttpConfigurer::disable);
+
         http.authorizeHttpRequests((authz) -> authz
                         .requestMatchers(new AntPathRequestMatcher("/health_check")).permitAll()
-                        .requestMatchers(new AntPathRequestMatcher("/users", "POST")).permitAll()
-                        .requestMatchers(new AntPathRequestMatcher("/users/**", "GET")).permitAll()
+                        .requestMatchers(new AntPathRequestMatcher("/actuator/**")).permitAll()
+
+                        .requestMatchers(new AntPathRequestMatcher("/users", "POST")).authenticated()
+                        .requestMatchers(new AntPathRequestMatcher("/users/**", "GET")).authenticated()
                         .requestMatchers("/**").access(
                                 new WebExpressionAuthorizationManager("hasIpAddress('127.0.0.1') or hasIpAddress('172.30.1.48')"))
                         .anyRequest().authenticated()
