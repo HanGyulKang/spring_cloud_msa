0564343eede9f9be95c86f894aff19e019460a40
HanGyulKang <gang6607@gmail.com>
2024-05-21 21:41:28 +0900

    gateway의 ip, port로 접속하는 것이 아닌 rest template 빈 등록 시 LoadBalanced 어노테이션을 달아서 서비스 명으로 통신할 수 있도록 한다.

--- user-service/src/main/java/inflern/study/userservice/config/UserServiceConfig.java
+++ user-service/src/main/java/inflern/study/userservice/config/UserServiceConfig.java
@@ -1,5 +1,6 @@
 package inflern.study.userservice.config;
 
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -14,6 +15,9 @@ public class UserServiceConfig {
     }
 
     @Bean
+    // 기존에 gateway ip, port를 사용해서 호출하던 것을
+    // 필요한 service name으로 호출할 수 있도록 도와 줌
+    @LoadBalanced  
     public RestTemplate getRestTemplate() {
         return new RestTemplate();
     }
